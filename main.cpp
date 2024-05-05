047b8519a89c0e8910fbe0c7cdd7e0d411b43b4b
Keana Cobarde <keanacobarde@gmail.com>
2024-05-05 16:07:16 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,35 @@
+// C867.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const string studentArray[] = { 
+        "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", 
+        "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", 
+        "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", 
+        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", 
+        "A5,Keana,Garciano-Whitton,kcobar2@wgu.edu,20,22,50,58,40,SOFTWARE" 
+    };
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << studentArray[i];
+    };
+    cout << "Hello World!\n";
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
