e9b337590918f68a4b2bfb38914885b259f54acf
Keana Cobarde <keanacobarde@gmail.com>
2024-05-05 22:28:38 -0500

    Creating student class within student.h and student.cpp

---
+++ student.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include <iostream>
+
+class Student {
+public:
+	int studentId;
+	string firstName;
+	string lastName;
+	string emailAddress;
+	int age;
+	int days[4];
+	string degreeProgram;
+};
