8bff18f47353ed2a34cdf62f3ae09caad553bbad
Keana Cobarde <keanacobarde@gmail.com>
2024-05-06 21:05:23 -0500

    Building student header file.

--- student.cpp
+++ student.cpp
@@ -1,13 +1,25 @@
-using namespace std;
+#pragma once
 #include <iostream>
+using namespace std;
 
 class Student {
-public:
-	int studentId;
-	string firstName;
-	string lastName;
-	string emailAddress;
-	int age;
-	int days[4];
-	string degreeProgram;
+	public:
+		const static int daysArraySize = 4;
+	public:
+		void setfirstName(string x)
+		{
+			firstname = x;
+		};
+		string getfirstName()
+		{
+			return firstname;
+		}
+	private: 
+		int studentId;
+		string firstName;
+		string lastName;
+		string emailAddress;
+		int age;
+		int days[daysArraySize];
+		string degreeProgram;
 };
--- student.h
+++ student.h
@@ -1,13 +1,52 @@
-using namespace std;
+#pragma once
 #include <iostream>
+#include "degree.h"
+using namespace std;
 
 class Student {
-public: 
+
+	/* SETTING ARRAY SIZE - MUST BE CONSTANT, C++ CANNOT DYNAMICALLY ASSIGN MEMEORY TO ARRAYS */
+public:
+	const static int daysArraySize = 4;
+
+	/* OBJECT SHAPE */
+private:
 	int studentId;
 	string firstName;
 	string lastName;
 	string emailAddress;
 	int age;
-	int days[4];
-	string degreeProgram;
+	int days[daysArraySize];
+	DegreeProgram degreeProgram;
+
+	/* CONSTRUCTORS AND DECONSTRUCTORS */
+public:
+	Student();
+	Student(int studentID, string firstName, string lastName, string emailAddress, int age, string days[], DegreeProgram degreeProgram);
+	~Student();
+
+	/* ACCESORS AND MUTATORS */
+public:
+	// ACCESSORS
+	string getstudentID();
+	string getfirstName();
+	string getlastName();
+	string getemailAddress();
+	int getAge();
+	string* getdays();
+	DegreeProgram getDegreeProgram();
+
+	//MUTATORS
+	void setstudentId(int ID);
+	void setfirstName(string firstname);
+	void setlastName(string lastname);
+	void setemailAddress(string emailAddress);
+	void setAge(int age);
+	void setDays(string days[]);
+	void setDegreeProgram(DegreeProgram degreeProgram);
+
+	static void printHeader();
+
+	void print();
+
 };
